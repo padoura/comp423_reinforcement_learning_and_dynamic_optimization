fa8557d0a72e4adfde0b4a750747ec7c1931378c
padoura <pantourakis.michail@gmail.com>
2023-06-14 08:19:10 +0300

    feat (python): Calculated action tree for first position preflop

--- poker_project/assignment1/python/random_agent.py
+++ poker_project/assignment1/python/random_agent.py
@@ -14,7 +14,7 @@ class RandomAgent:
         '''
         self.np_random = np_random
         self.use_raw = True
-        self.num_actions = num_actions
+        self.num_actions = num_actions # TODO: obsolete, to be deleted
 
     def step(self, state):
         ''' Completely random agent
@@ -150,3 +150,98 @@ class RandomAgent:
                 flop_probabilities[key][public_key] = flop_frequencies[key][public_key] / sum(flop_frequencies[key].values())
 
         return [ win_probabilities, loss_probabilities, flop_probabilities ]
+    
+    @staticmethod
+    def calculate_game_tree():
+
+
+        # | Index          | # Enum |Meaning                                                                        |
+        # | ---------------|--------|-------------------------------------------------------------------------------|
+        # | 'position'     |   2    |Position of player 'first'/'second'                                            |
+        # | 'my_chips'     |   5    |chips placed by our agent so far                                               |
+        # | 'other_chips'  |   3    |difference in chips placed between adversary and our agent so far              |
+        # | 'hand'         |   5    |Rank of hand: T ~ A as first public card                                       |
+        # | 'public_cards' |   16   |Rank of public cards in alphabetical order e.g. 'AK' or 'none' if not shown yet|
+
+        # positions = {'first', 'second'}
+        # chips = {'0.5', '1.5', '2.5', '3.5', '4.5'}
+        public_cards_set = {'none'}
+        for public_card1 in Dealer.RANK_LIST:
+            for public_hand2 in Dealer.RANK_LIST:
+                public_cards_set.add(''.join(sorted(public_card1 + public_hand2)))
+
+        # legal_action_sequences = Judger.get_legal_sequences_of_actions()
+
+        game_tree = {}
+
+        ############## position == 'first' #################
+        position = 'first'
+        # preflop @ chips [0.5, 0.5]
+        my_chips = 0.5
+        other_chips = 0
+        for my_action in ['bet', 'check']:
+            RandomAgent.add_or_update_key(game_tree, position, my_chips, other_chips, my_action)
+        # preflop @ chips [0.5, 1.5] or [1.5, 2.5]
+        other_chips = 1
+        for my_chips in [0.5, 1.5]:
+            for my_action in ['fold', 'bet']:
+                RandomAgent.add_or_update_key(game_tree, position, my_chips, other_chips, my_action)
+
+
+        return game_tree
+    
+
+    @staticmethod
+    def add_or_update_key(game_tree, position, my_chips, other_chips, my_action):
+        key = position + '_' + str(my_chips) + '_' + str(other_chips) + '_' + my_action
+        if key not in game_tree: game_tree[key] = []
+        if (position == 'first' and other_chips == 0 and my_action == 'bet'):
+            new_my_chips = my_chips + 1
+            prob = 1/3 # random selection between 'fold', 'raise', 'bet'
+            #### other_action == 'fold' ####
+            is_terminal = True
+            new_other_chips = -1
+            reward = my_chips
+            game_tree[key].append( (prob, position, new_my_chips, new_other_chips, is_terminal, reward, 0, 0)  )
+            #### other_action == 'bet' ####
+            is_terminal = False
+            new_other_chips = 0
+            reward = 0
+            game_tree[key].append( (prob, position, new_my_chips, new_other_chips, is_terminal, reward, 0, 0)  )
+            #### other_action == 'raise' ####
+            is_terminal = False
+            new_other_chips = 1
+            reward = 0
+            game_tree[key].append( (prob, position, new_my_chips, new_other_chips, is_terminal, reward, 0, 0)  )
+        elif (position == 'first' and other_chips == 1 and my_action == 'bet'):
+            new_my_chips = my_chips + 1
+            prob = 1 # random agent has finished his move
+            is_terminal = False
+            new_other_chips = 0
+            reward = 0
+            game_tree[key].append( (prob, position, new_my_chips, new_other_chips, is_terminal, reward, 0, 0)  )
+        elif (position == 'first' and my_action == 'fold'):
+            new_my_chips = my_chips
+            prob = 1 # random agent has finished his move
+            is_terminal = True
+            new_other_chips = 1
+            reward = -my_chips
+            game_tree[key].append( (prob, position, new_my_chips, new_other_chips, is_terminal, reward, 0, 0)  )
+        elif (position == 'first' and my_action == 'check'):
+            new_my_chips = my_chips
+            prob = 0.5 # random selection between 'check', 'raise'
+            #### other_action == 'check' ####
+            is_terminal = False
+            new_other_chips = 0
+            reward = 0
+            game_tree[key].append( (prob, position, new_my_chips, new_other_chips, is_terminal, reward, 0, 0)  )
+            #### other_action == 'raise' ####
+            is_terminal = False
+            new_other_chips = 1
+            reward = 0
+            game_tree[key].append( (prob, position, new_my_chips, new_other_chips, is_terminal, reward, 0, 0)  )
+
+# import json
+
+# with open("game_tree.json", "w") as write_file:
+#     json.dump(RandomAgent.calculate_game_tree(), write_file, indent=4, sort_keys=True)
