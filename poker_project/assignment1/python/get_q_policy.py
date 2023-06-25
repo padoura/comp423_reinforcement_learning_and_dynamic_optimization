51820645252d698b95a0313b47800ff2af0516e8
Michail Pantourakis <pantourakis.michail@gmail.com>
2023-06-25 12:20:13 +0300

    chore: Ran Q Learning for 10^7 episodes

--- poker_project/assignment1/python/get_q_policy.py
+++ poker_project/assignment1/python/get_q_policy.py
@@ -3,12 +3,12 @@ import os
 
 q_trained_model = None
 print("Loading pretrained model...")
-with open(os.path.dirname(os.path.abspath(__file__))+'\\q_test.json') as json_file:
+with open(os.path.dirname(os.path.abspath(__file__))+'\\q_random_model.json') as json_file:
     q_trained_model = json.load(json_file)
 
 Q = q_trained_model['Q']
 
 Q_policy = {state: max(Q[state], key=Q[state].get) for state in Q}
 
-with open(os.path.dirname(os.path.abspath(__file__))+'\\q_policy_test.json', 'w') as json_file:
+with open(os.path.dirname(os.path.abspath(__file__))+'\\q_policy_random_model.json', 'w') as json_file:
     json.dump(Q_policy, json_file, indent=4, sort_keys=True)
