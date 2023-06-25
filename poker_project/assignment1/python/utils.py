e72faa84a23eb900e6db25c53c9586d87bbfd0ea
Michail Pantourakis <pantourakis.michail@gmail.com>
2023-06-26 02:03:44 +0300

    feat (python): Finished plotting moving averages

--- poker_project/assignment1/python/utils.py
+++ poker_project/assignment1/python/utils.py
@@ -1,3 +1,25 @@
 def try_key_initialization(dictionary, key, initial_value):
     if key not in dictionary:
         dictionary[key] = initial_value
+
+def get_moving_average(arr, window_size):
+    i = 1
+    window_sum = sum(arr[:window_size])
+    window_average = window_sum / window_size
+    moving_averages = [window_average]
+    while i < len(arr) - window_size + 1:
+        
+        # Update window sum by subtracting first element and adding new element
+        window_sum += arr[i + window_size - 1] - arr[i-1]
+    
+        # Calculate the average of current window
+        window_average = window_sum / window_size
+        
+        # Store the average of current
+        # window in moving average list
+        moving_averages.append(window_average)
+        
+        # Shift window to right by one position
+        i += 1
+    
+    return moving_averages
